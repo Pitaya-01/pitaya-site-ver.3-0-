7f0cc6638766734283ee59c67e6e75362e63238d
Pitaya <125665899+Pitaya-01@users.noreply.github.com>
2023-05-20 13:57:35 -0300

    Add files via upload

---
+++ pitaya/index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pitaya</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+    crossorigin="anonymous"></script>
+
+</head>
+
+
+<body style="font-family: 'Trebuchet MS', 'Helvetica', 'sans-serif'">
+
+<!-- NAVBAR -->
+<header>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #ffff; box-shadow: 0px 4px 4px #FF548A; border-bottom-left-radius: 30px; border-bottom-right-radius: 30px;">
+    <div class="container px-1">
+      <a class="navbar-brand" style="margin-left: -7%" href="/">
+        <span style="color:#000000; font-size:26px; font-weight:bold; letter-spacing: 1px;"><img src="imgs/pitayanav.png" width="35%"></span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+          class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-4" >
+          <li class="nav-item"><a class="nav-link" id="link"  href="/">PRODUTO</a></li>
+          <li class="nav-item"><a class="nav-link" id="link" href="/code">OBJETIVOS</a></li>
+          <li class="nav-item"><a class="nav-link" id="link" href="/pricing">EQUIPE</a></li>
+          <li class="nav-item"><a class="nav-link" id="link" href="/about">CONTATO</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+</header>
+
+<!-- NAVBAR-->
+
+<!-- HERO SECTION -->
+<section>
+
+<div class="container-fluid" id="hero" style="margin-top: -2%;padding-top: 5%;justify-content: center; box-shadow: 0px 4px 4px #FF548A; height: 630px; background: linear-gradient(90.05deg, rgba(255, 255, 255, 0) 27.33%, rgba(255, 210, 224, 0.489436) 36.18%, #FFA3C0 45.42%, #FF88AD 51.67%, #FF548A 63.29%, #FF548A 70.48%, #FF548A 81.54%, #FF548A 97.58%);  ">
+
+<div class="row" style="margin-left: 0%">
+
+  <div class="col-md-7">
+    <img src="imgs/logo2.png" width="53%" alt="">
+  </div>
+
+  <div class="col-md-4" style="padding-top: 5%; color: #ffffff;">
+  <figure class="text-left">
+    <h1 class="display-2"><u>O que é PITAYA?</u></h1>
+  </figure>
+  <figure class="text-left fs-2">
+  PITAYA é uma equipe de desenvolvedores criada com o objetivo de desenvolver tecnologias voltadas para o cotidiano das pessoas.
+  </figure>
+
+  </div>
+</div>
+
+</div>
+
+</section>
+<!-- HERO SECTION -->
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+<!-- WRAPPER ----------------------------------------------------------->
+<div class="wrapper" style="padding-left: 15%;">
+<!-- WRAPPER ----------------------------------------------------------->
+
+
+<!-- PRODUTO -->
+
+<section class="container-fluid">
+
+
+
+<div class="row" style="margin-left: 0%">
+
+
+    <img class="col-md-4" src="imgs/produto.jpg" width="10"  style="  border-style: solid;
+  border-color: #FF548A ;box-shadow: 0px 0px 70px 6px #FF548A; border-radius: 20px;" alt="">
+
+
+  <div class="col-md-7" style="color: #000000; padding-left: 4%;">
+  <figure class="text-left">
+    <h1 class="display-6" style="color: #FF548A;"><u>ALIMENTADOR AUTOMÁTICO PARA PETS</u></h1>
+  </figure>
+  <figure class="text-left fs-2">
+  Este produto tem a finalidade de facilitar a alimentação dos animais e economizar o tempo dos pais de pets. Através de um aplicativo 
+  que você pode baixar no seu celular diretamente da play store, você poderá liberar a ração e água do pet da distância que você estiver,  
+  podendo também definir as quantidades necessárias, e tudo isso fora ou dentro do mesmo ambiente que o animal. 
+  </figure>
+  </div>
+
+</div>
+
+<br>
+<br>
+<br>
+
+<div class="row" style="margin-left: 0%">
+
+<div class="col-md-11" style="color: #000000;">
+  <figure class="text-left">
+    <h1 class="display-6" style="color: #FF548A;"><u>COMO FUNCIONA?</u></h1>
+  </figure>
+  <figure class="text-left fs-2">
+  O equipamento será construído por meio de canos e mangueiras, os quais terão sensores e outras peças do Arduino. Esse equipamento 
+  pode estar em vários ambientes, como: em casa, hotéis de pets, adestramentos e petshops.
+  </figure>
+  </div>
+
+</div>
+
+
+</section>
+
+<!-- PRODUTO -->
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<!-- PORQUE DEVO TER UM ALIMENTADOR? -->
+
+<section class="container-fluid">
+
+
+<div class="row" style="margin-left: 0%">
+
+<div class="col-md-11" style="color: #000000;">
+  <figure class="text-left">
+    <h1 class="display-6" style="color: #FF548A;"><u>POR QUE DEVO TER UM ALIMENTADOR AUTOMÁTICO</u></h1>
+  </figure>
+  <figure class="text-left fs-2">
+  Todos os donos que amam os seus pets, sabem da importância de alimentá-los bem. Mas nem sempre conseguem seguir horários regulados e 
+  saudáveis, seja por falta de tempo ou viagens. É por isso que a nossa equipe criou o (nome do produto): um equipamento que fornece 
+  ração aos animais atráves do celular.
+  </figure>
+  </div>
+
+</div>
+
+
+
+</section>
+
+
+<br>
+<br>
+
+
+<!-- PORQUE DEVO TER UM ALIMENTADOR? -->
+
+<div class="row" style="margin-left: 2%">
+
+
+    <img class="col-md-4" src="imgs/alimentador.webp"  style="  border-style: solid;
+  border-color: #FF548A ;box-shadow: 0px 0px 70px 6px #FF548A; border-radius: 20px;" alt="">
+
+
+  <div class="col-md-7" style="color: #000000; padding-left: 4%;">
+  <figure class="text-left fs-2">
+    <p>
+  Tecnologia revolucionária e bem útil. É claro que o ideal é você interagir diretamente com o seu pet e este aparelho não deve 
+  substituir um dono. 
+  </p>
+  <p>
+  Mas em uma viagem prolongada ou em um dia de trabalho mais corrido, com alguns simples toques em seu smartphone, você disponibiliza 
+  a quantidade certa de ração na vasilha do pet.Aos que têm cachorro, o adestrador Glauco Lima já alerta que os animais que nunca ficaram sozinhos vão demorar cerca de três dias 
+  para se acostumar. 
+  </p>
+  </figure>
+  </div>
+
+</div>
+
+<br>
+<br>
+
+<div class="row" style="margin-left: 1%">
+
+<div class="col-md-11" style="color: #000000;">
+  <figure class="text-left fs-2">
+  “O cachorro que nunca ficou sozinho vai ficar uns três dias sem se alimentar direito por estar em uma situação nova. 
+  Mas aqueles mais acostumados com a ausência do dono só vão se sentir como se estivessem “mudando da casa da mãe para a casa da avó”.
+  </figure>
+  </div>
+
+</div>
+
+<!-- PORQUE DEVO TER UM ALIMENTADOR? -->
+
+
+
+
+<footer>
+
+
+
+</footer>
+
+</div> <!-- fechamento wrapper -->
+</body>
+</html>
+
+
+<!--
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+ -->
